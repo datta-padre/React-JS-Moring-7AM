aeae1da7d63840e4164a9e39812064e43be512ad
Datta Padre <datta.padre@gmail.com>
2026-05-25 09:04:54 +0530

    day-9

---
+++ day-9/src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useState , useEffect } from "react";
+
+function App(){
+
+  const [userName , setUserName] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+
+    console.log("New Data = ", data);
+
+  },[data]);
+
+  function submit(){
+
+    console.log(userName);    
+    const newArrya = [...data,userName];
+    setData(newArrya);        
+  }
+
+  return(
+    <>
+    <h1>Welcome Back</h1>
+    <br /><br />
+    Enter UserName <br />
+    <input type="text" onChange={(e)=>setUserName(e.target.value)} />
+    <br /><br />
+    <button onClick={submit}>Submit</button>
+    <br /><br />
+
+    <p></p>
+    </>
+  )
+}
+
+
+export default App;
+
+
+
+
+
+
+
